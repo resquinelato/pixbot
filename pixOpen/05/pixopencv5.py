4ce4493dca0c931d7a3fea8d9adcd67e6325280f
Rodrigo Esquinelato <resquinelato@gmail.com>
2024-03-18 23:07:00 -0300

    008

---
+++ pixOpen/05/pixopencv5.py
@@ -0,0 +1,37 @@
+import cv2 as cv
+import numpy as np
+import os
+from time import time
+from windowcapture import WindowCapture
+from vision import findClickPositions
+
+#WindowCapture.list_window_names()
+#exit()
+
+# initialize the WindowCapture class
+wincap = WindowCapture('Explorador de Arquivos')
+
+
+loop_time = time()
+while(True):
+
+    # get an updated image of the game
+    screenshot = wincap.get_screenshot()
+    # precisa ser um argumento amtematico no imashow
+    screenshot = np.array(screenshot)
+    #corrige a coloraçao de troca de RGB(nao ta precisando)
+    #screenshot = cv.cvtColor(screenshot,cv.COLOR_RGB2BGR)
+
+
+    #cv.imshow('Computer Vision', screenshot)
+    findClickPositions('', screenshot, 0.25 , debug_mode=None)
+
+    # msotra o FPS de execução (pode ser melhorado)
+    print('FPS {}'.format(1 / (time() - loop_time)))
+    loop_time = time()
+
+    # press 'q' with the output window focused to exit.
+    # waits 1 ms every loop to process key pressesgi
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
---
+++ pixOpen/05/vision.py
@@ -0,0 +1,78 @@
+#copia de pixopencv3_2.py
+import cv2 as cv
+import numpy as np
+
+
+
+
+def findClickPositions(needle_img_path, haystack_img, threshold=0.25, debug_mode=None):
+
+    #metodo que a foto será tratada
+    needle_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)
+
+    needle_w = needle_img.shape[1]
+    needle_h = needle_img.shape[0]
+
+    #método de comparação das imagens
+    method = cv.TM_CCOEFF_NORMED
+    result = cv.matchTemplate(haystack_img, needle_img, method)
+
+    locations = np.where(result >= threshold)
+    #faz o reverço da matriz pra ficar mais legivel e arruanja em tuplas
+    locations = list(zip(*locations[::-1]))
+
+    #agrupando os retangulos criando lista de [x,y,w,h]
+    rectangles = []
+    for loc in locations:
+        rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
+        rectangles.append(rect)
+        #para que retangulos unico tambewm sejam escolhidos
+        rectangles.append(rect)
+
+    #no metodo rectangles  1 eh o tento que se repete, 0.5 eha  distancia tolerada
+    rectangles, weights = cv.groupRectangles(rectangles, groupThreshold=1, eps=0.5)
+
+    #sem o metodo anterior funciona como a função de antes(se tirar o len)
+    points = []
+    if len(rectangles):
+    #if rectangles:
+        print('Found needle.')
+
+        line_color = (0, 255, 0)
+        line_type = cv.LINE_4
+        marker_color = (0, 0, 225)
+        marker_type =  cv.MARKER_CROSS
+
+        # Loop over all the locations and draw their rectangle
+        for (x, y, w, h) in rectangles:
+            # Determine centro das posicoes
+            center_x = x+int(w/2)
+            center_y = y+int(h/2)
+            # salvando pontos
+            points.append((center_x, center_y))
+
+            if debug_mode == 'rectangles':
+                # Determine the box position
+                top_left = (x, y)
+                bottom_right = (x + w, y + h)
+                # Draw the box
+                cv.rectangle(haystack_img, top_left, bottom_right, color=line_color, 
+                             lineType=line_type, thickness=2)
+
+            elif debug_mode == 'points':
+                # Draw the center point
+                cv.drawMarker(haystack_img, (center_x, center_y), 
+                              color=marker_color, markerType=marker_type, 
+                              markerSize=40, thickness=2)
+
+    if debug_mode:
+        cv.imshow('Matches', haystack_img)
+        #cv.waitKey()
+        #cv.imwrite('result_click_point.jpg', haystack_img)
+
+    return points
+
+
+
+pontos = findClickPositions('img/needle5.png','img/palheiro2.png', threshold=0.3, debug_mode='points')
+print(pontos)
